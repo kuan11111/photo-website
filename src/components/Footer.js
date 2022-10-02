a8d9aaeb68ce3ada4d148133abbe36ced5d6ff61
Sandy Lu <ethanlu0905@gmail.com>
2022-10-02 12:42:43 +0800

    fix

---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="footer">Sandy Lu 2022©</div>;
+};
+
+export default Footer;
+
---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about" style={{ minHeight: "100vh" }}>
+      <h1>ABOUT</h1>
+      <span>
+        You can use the web to search the picture what you want to. Also, you
+        can download it with the button.
+      </span>
+      <span>
+        If you have any problem,please contact my email:ethanlu0905@gmail.com
+      </span>
+    </div>
+  );
+};
+
+export default About;
+
